7fbfee0ea3eb672ae5a4c761b8528c4449071bf4
Ishimwe Delphin <delphinishimwe55@gmail.com>
2025-08-04 14:32:42 +0200

    Update githubService.js

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -3,7 +3,7 @@ const apiKey = import.meta.env.VITE_APP_GITHUB_API_KEY;
 
 export const fetchUserData = async (username) => {
   const res = await fetch(`https://api.github.com/users/${username}`, {
-    method: 'GET'
+    method: 'get'
     headers: {
       Authorization: `token ${apiKey}`
     }
